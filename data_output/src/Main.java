1fd6d9102f4403504c98f468c24cc66da312d665
Ryan <ryandavid2018x@gmail.com>
2025-03-03 09:29:57 -0300

    Adding new knowledge.

---
+++ data_output/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		// Printing inter values:
+		/*int x = 32;
+		System.out.println(x);*/
+		
+		//Printing floating values:
+		/*double y = 64.84234;
+		System.out.println(y);
+		System.out.printf("%.2f\n", y);
+		System.out.printf("%.3f\n", y);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f\n", y);
+		System.out.print(y);*/
+		
+		// Printing concatenated values:
+		/*String name = "Ryan";
+		System.out.println(name + " is too beaultiful!");*/
+			
+		// Printing concatenated values v2:
+		/*String name = "David";
+		double cash = 1200.433;
+		System.out.printf("%s have U$%.2f dollars\n", name, cash);*/
+	
+		// 
+	}
+
+}
